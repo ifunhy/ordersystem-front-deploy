3bf2d4780f5acb2af309971c8abfecc02ac8e323
ifunhy <guswl8296@gmail.com>
2025-08-29 10:03:10 +0900

    Feat: ordersystem-front-deploy

---
+++ src/components/practice/6.StoreTestComponentB.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>store componentB</h1>
+    <p>지역변수값 count : {{ count }}</p>   <!-- data()에 선언된 count값 삽입됨-->
+    <p>전역store변수값 count : {{ getStoreCount }}</p>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            count: 0,
+        }
+    },
+
+    computed:{
+        getStoreCount(){    // count : {{ getStoreCount }}처럼 computed 내 함수는 변수처럼 사용 가능
+            return this.$store.getters.getCount;
+        }
+    },
+}
+</script> 
